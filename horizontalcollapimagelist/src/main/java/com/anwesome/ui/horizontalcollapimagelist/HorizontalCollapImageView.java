024f1f5856244d017e3d7f44d5db71df61f1296e
Anwesh43 <anweshthecool0@gmail.com>
2017-06-05 01:16:18 +0530

    adding collap image

--- horizontalcollapimagelist/src/main/java/com/anwesome/ui/horizontalcollapimagelist/HorizontalCollapImageView.java
+++ horizontalcollapimagelist/src/main/java/com/anwesome/ui/horizontalcollapimagelist/HorizontalCollapImageView.java
@@ -3,6 +3,7 @@ package com.anwesome.ui.horizontalcollapimagelist;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.MotionEvent;
 import android.view.View;
@@ -13,7 +14,7 @@ import android.view.View;
 
 public class HorizontalCollapImageView extends View{
     private Bitmap bitmap;
-    private int time = 0,w,h;
+    private int time = 0,w,h,color = Color.parseColor("#3949AB");
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     public HorizontalCollapImageView(Context context, Bitmap bitmap) {
         super(context);
@@ -25,6 +26,7 @@ public class HorizontalCollapImageView extends View{
             h = canvas.getHeight();
             bitmap = Bitmap.createScaledBitmap(bitmap,4*w/5,4*h/5,true);
         }
+        paint.setColor(color);
         time++;
     }
     public void update(float factor) {
@@ -32,8 +34,31 @@ public class HorizontalCollapImageView extends View{
     }
     public boolean onTouchEvent(MotionEvent event) {
         if(event.getAction() == MotionEvent.ACTION_DOWN) {
-            
+
         }
         return true;
+    }
+    private class CollapImage {
+        private float x,y,scale = 0;
+        public CollapImage(float x,float y) {
+            this.x = x;
+            this.y = y;
+        }
+        public void draw(Canvas canvas) {
+            int bw = bitmap.getWidth()/2,bh = bitmap.getHeight()/2;
+            paint.setStrokeWidth(bw/20);
+            canvas.save();
+            canvas.translate(x,y);
+            canvas.scale(scale,1);
+            canvas.drawBitmap(bitmap,-bw,-bh,paint);
+            canvas.drawLine(-bw*scale,-bh,-bw*scale,bh,paint);
+            canvas.drawLine(bw*scale,-bh,bw*scale,bh,paint);
+            canvas.drawLine(-bw*scale,-bh,bw*scale,-bh,paint);
+            canvas.drawLine(-bw*scale,bh,bw*scale,bh,paint);
+            canvas.restore();
+        }
+        public void update(float factor) {
+            scale = factor;
+        }
     }
 }
