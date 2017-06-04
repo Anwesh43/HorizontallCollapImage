0aefcad190945af0c9822220634505bb922ed614
Anwesh43 <anweshthecool0@gmail.com>
2017-06-05 02:15:26 +0530

    Using HorizontalCollapImageList in app

--- app/src/main/java/com/anwesome/ui/horizontalcollapimagelistdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/horizontalcollapimagelistdemo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.anwesome.ui.horizontalcollapimagelistdemo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.anwesome.ui.horizontalcollapimagelist.HorizontalCollapImageList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.stp);
+        HorizontalCollapImageList horizontalCollapImageList = new HorizontalCollapImageList(this);
+        for(int i=0;i<6;i++) {
+            horizontalCollapImageList.addImage(bitmap);
+        }
+        horizontalCollapImageList.show();
     }
 }
