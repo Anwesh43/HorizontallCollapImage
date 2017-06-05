2f64b93c321e77c37d416f9ebf07ecd48cab26e2
Anwesh43 <anweshthecool0@gmail.com>
2017-06-05 09:01:46 +0530

    Using OnImageCollapListener in Activity

--- app/src/main/java/com/anwesome/ui/horizontalcollapimagelistdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/horizontalcollapimagelistdemo/MainActivity.java
@@ -4,8 +4,10 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.anwesome.ui.horizontalcollapimagelist.HorizontalCollapImageList;
+import com.anwesome.ui.horizontalcollapimagelist.OnImageCollapListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +18,18 @@ public class MainActivity extends AppCompatActivity {
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.stp);
         HorizontalCollapImageList horizontalCollapImageList = new HorizontalCollapImageList(this);
         for(int i=0;i<6;i++) {
-            horizontalCollapImageList.addImage(bitmap);
+            final int index = i+1;
+            horizontalCollapImageList.addImage(bitmap, new OnImageCollapListener() {
+                @Override
+                public void onOpen() {
+                    Toast.makeText(MainActivity.this, String.format("%d opened",index), Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onClose() {
+                    Toast.makeText(MainActivity.this, String.format("%d closed",index), Toast.LENGTH_SHORT).show();
+                }
+            });
         }
         horizontalCollapImageList.show();
     }
